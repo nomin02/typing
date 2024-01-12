fd3ed652536bee7447eb043c71b45f383cbf46db
nomin02 <flvlclq89@gmail.com>
2024-01-12 23:41:06 +0800

    오늘은 여기까지

--- start.py
+++ start.py
@@ -1,1 +1,11 @@
-print("hello world!!")
+
+succ_count = 0;fail_count = 0
+typing_list = ['무궁화', '꽃이', '피었습니다.', 'The']
+for list_item in typing_list:
+    print(list_item)
+    typing_input = input(">>:")
+    if list_item == typing_input:
+        succ_count +=1
+    else:
+        fail_count +=1
+    print('타이핑 연습 종료! 정답: ',succ_count,' 오류: ',fail_count)
