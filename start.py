a12c68ed068941d630abe204cf1e7f8d9df52ec7
nomin02 <flvlclq89@gmail.com>
2024-01-14 15:56:57 +0800

    어제꺼

--- start.py
+++ start.py
@@ -16,6 +16,13 @@ def countUP():
 label = tkinter.Label(window, text = "Typing Practice", font = ("Arial", 30))
 label.pack()
 
-button = tkinter.Button(window, text = "자리 연습", overrelief="solid", width = 15, repeatdelay=1000, repeatinterval=100, padx=100, pady = 30, bg = "gray", fg = "black")
-button.pack()
+b1 = tkinter.Button(window, text = "자리 연습", overrelief="solid", width = 15, repeatdelay=1000, repeatinterval=100, padx=100, pady = 30, bg = "gray", fg = "black")
+b2= tkinter.Button(window, text = "단어 연습", overrelief="solid", width = 15, repeatdelay=1000, repeatinterval=100, padx=100, pady = 30, bg = "gray", fg = "black")
+b3 = tkinter.Button(window, text = "짧은 글 연습", overrelief="solid", width = 15, repeatdelay=1000, repeatinterval=100, padx=100, pady = 30, bg = "gray", fg = "black")
+b4 = tkinter.Button(window, text = "긴 글 연습", overrelief="solid", width = 15, repeatdelay=1000, repeatinterval=100, padx=100, pady = 30, bg = "gray", fg = "black")
+
+b1.pack()
+b2.pack()
+b3.pack()
+b4.pack()
 window.mainloop()
